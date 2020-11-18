0a1710357ac8e013ec1132ec5ddc50c3bd5e1a84
Alok Kumar <alokkola21@gmail.com>
2020-11-18 15:00:03 +0530

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./HomePageStyle.css";
 
@@ -14,9 +15,9 @@ class HomePage extends React.Component {
           <br />
           <br />
           <br />
-          <a className="homepage-link" href="/conflist">
+          <Link className="homepage-link" to="/conflist">
             Explore All
-          </a>
+          </Link>
         </div>
       </div>
     );
