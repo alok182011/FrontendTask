8837bc9263b4a163f2aa39e9e14dc7f163de5239
Alok Kumar <alokkola21@gmail.com>
2020-11-18 15:29:54 +0530

    Update DisplayTile.jsx

--- src/components/DisplayTile.jsx
+++ src/components/DisplayTile.jsx
@@ -6,9 +6,9 @@ const DisplayTile = ({ name, poster, date, type, place, confurl }) => (
   <div className="displaytile-container">
     <div className="displaytile-text">
       <h3 className="displaytile-title">{name}</h3>
-      <h5>{date}</h5>
-      <h5>Venue: {place}</h5>
-      <h5>Entry Type: {type}</h5>
+      <h4>{date}</h4>
+      <h4>Venue: {place}</h4>
+      <h4>Entry Type: {type}</h4>
       <h5>
         <a href={confurl} target="blank">
           Visit Conference Website
